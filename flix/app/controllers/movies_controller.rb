f3525f27b6c36f7cd77418e5d2cfde988620a3ef
TyJaYo <tylerjamesyoung@gmail.com>
2022-10-23 12:09:58 -0600

    Forms: Editing Records

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -6,4 +6,17 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find_by(title: params[:title])
   end
+
+  def edit
+    @movie = Movie.find_by(title: params[:title])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params =
+      params.require(:movie)
+            .permit(:title, :description, :rating, :released_on, :total_gross)
+    @movie.update(movie_params)
+    redirect_to movie_path(@movie.title)
+  end
 end
--- flix/config/routes.rb
+++ flix/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'movies#index'
   get 'movies' => 'movies#index'
   get 'movies/:title' => 'movies#show', as: 'movie'
+  get 'movies/:title/edit' => 'movies#edit', as: 'edit_movie'
+  patch 'movies/:id' => 'movies#update'
 end
